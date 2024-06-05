01ee51cdc5aca326105e76273641ccd0d2e124eb
STEPHENOSHATEYE <155460166+STEPHENOSHATEYE@users.noreply.github.com>
2024-06-05 23:11:03 +0100

    add Event Listener to run game function anytime a button is clicked

--- index.js
+++ index.js
@@ -1,9 +1,35 @@
-//Initialize playgame() function;
-function playGame(){
-    //Initialize computer and human score to 0;
-    let computerScore = 0;
-    let humanScore = 0;
+const buttons = document.querySelectorAll('button');
+const div = document.getElementById('scoreBoard-container');
+const roundResult = document.getElementById('info');
 
+let globalHumanChoice = '';
+//Initialize the number of Round to 1, so it iterates after each round;
+let roundCounter = 1;
+//Initialize computer and human score to 0;
+let computerScore = 0;
+let humanScore = 0;
+
+function checkRound(event){
+    if (roundCounter < 6){
+        //Check if roundCounter is less than or equal 5; then run playGame
+        //and pass the event from the eventlistener as an argument in the function;
+        playGame(event)
+    }else{
+        //Display result of the game 
+        alert((humanScore>computerScore)?
+        "Congratulations, you won this game".toUpperCase():
+        "Oppps, you lose this game, try again".toUpperCase());
+        buttons.forEach((button) => {
+            button.removeEventListener('click',()=> {});
+        });
+    };
+};
+
+buttons.forEach((button) => {
+    button.addEventListener("click", checkRound)
+})
+//Initialize playgame() function;
+function playGame(event){
     function makeComputerChoice(){
         //An array of rock, paper, scissors where computer makes choices
         let rps = ['rock', 'paper', 'scissors'];
@@ -13,7 +39,7 @@ function playGame(){
 
     function getHumanChoice(){
         //Prompt user to input rock, paper, scissors;
-        let humanChoice = (prompt('Enter Rock, Paper or Scissors')).toLowerCase();
+        let humanChoice = globalHumanChoice;
         //If user input rock, paper, scissors corrrectly, return humanChoice
         if ((humanChoice.toUpperCase() === "ROCK") || 
             (humanChoice.toUpperCase() === "PAPER") || 
@@ -55,59 +81,51 @@ function playGame(){
         }else{
             outcome = 'Human choice'
         }
-        
         return outcome;
     }
  
-    //Initialize round to 1, so the games starts at round 1;
-    let roundCounter = 1;
-    //Set the number of rounds to be played;
-    const numberOfRounds = 5;
 
-    //While round counter is less than or equal 5, playGame;
-    while (roundCounter <= numberOfRounds){
-        console.log(`Round ${roundCounter} begin!`);
+    //Play next round while replayRound is force, replay if replayRound is true;
+    let replayRound = false;
+    let computerChoice = makeComputerChoice();
+    const humanChoice = event.target.className;
+
+    
+    roundOutcome = playRound(computerChoice,humanChoice);
+    switch (roundOutcome.toLowerCase()){
+        case ('tie'):
+            console.log(`It's a tie, no winner replay round`)
+            roundResult.textContent = `It's a tie, no winner replay round`;
+            //If round outcome is a tie, set replayRound to true
+            replayRound = true;
+        break;
+
+        case ("win"):
+            console.log('You won this round!!');
+            roundResult.textContent = 'You won this round';
+            humanScore++;
+        break;
+
+        case ("lose"):
+            console.log('You lose this round!!');
+            roundResult.textContent = 'You lose this round!!';
+            computerScore++;
+        break;
+
+        case ("human choice"):
+            console.log("You've inputted a wrong choice, try again");
+            roundResult.textContent = "You've inputted a wrong choice, try again";
+            replayRound = true; 
+        break;
+    }    
+
+    //Update and Display score to the page
+    div.textContent = (`human:${humanScore} : computer:${computerScore}`);
         
-        //Play next round while replayRound is force, replay if replayRound is true;
-        let replayRound = false;
-        let computerChoice = makeComputerChoice();
-        let humanChoice = getHumanChoice();
-
-        
-        roundOutcome = playRound(computerChoice,humanChoice);
-        switch (roundOutcome.toLowerCase()){
-            case ('tie'):
-                console.log(`It's a tie, no winner replay round`)
-                //If round outcome is a tie, set replayRound to true
-                replayRound = true;
-            break;
-
-            case ("win"):
-                console.log('You won this round!!');
-                humanScore++;
-            break;
-
-            case ("lose"):
-                console.log('You lose this round!!');
-                computerScore++;
-            break;
-
-            case ("human choice"):
-                console.log("You've inputted a wrong choice, try again");
-                replayRound = true; 
-            break;
-        }    
-
-        console.log(`human:${humanScore} : computer:${computerScore}`);
-        
-        //Increment round counter if replay round is false, else don't increment;
-        if (!replayRound){
-            roundCounter++;
-        }
-    //End while loop
+    // Increment round counter if replay round is false, else don't increment;
+    if (!replayRound){
+        ++roundCounter;
     }
-    //Display result of the game 
-    console.log((humanScore>computerScore)?
-    "Congratulations, you won this game".toUpperCase():
-    "Oppps, you lose this game, try again".toUpperCase());
+
+    //End while loop
 }
